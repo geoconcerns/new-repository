fa5f6792b9bdde156a7098e39b3cfd6b7715495c
griffinj@lafayette.edu <griffinj@lafayette.edu>
2016-09-07 14:16:45 -0400

    #172 Decoupling all provisioning tasks from the Vagrant Box except for the creation and deployment of the GeoConcerns demo. app.; Using geoconcerns/geo-concerns-vagrant-base as the Base Box

--- install_scripts/geo-concerns.sh
+++ install_scripts/geo-concerns.sh
@@ -5,7 +5,6 @@ echo "Creating a stock Geo Concerns app"
 # Create a curation-concerns app
 rails new geo-concerns-demo --skip-spring
 cd geo-concerns-demo
-sudo gem install simple_mapnik
 echo 'gem "geo_concerns",  "~>0.0.7"' >> Gemfile
 bundle install
 yes Y | rails generate curation_concerns:install
@@ -20,5 +19,5 @@ yes | cp -rf $SHARED_DIR/install_scripts/Rakefile .
 # Make solr core nonpersistent. Causes errors when solr-wrapper is restarted.
 sed -i 's/persist: true/persist: false/g' .solr_wrapper
 
-# Run the servers
-rake demo:servers
+# Run the setup
+rake demo:setup
